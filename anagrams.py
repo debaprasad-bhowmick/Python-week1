b9fe2668d99ffdb798a76af8270ed0e0c8ef83de
debaprasad-bhowmick <debaprasadbhowmik5@gmail.com>
2026-02-14 14:10:59 +0530

    don

--- anagrams.py
+++ anagrams.py
@@ -1,12 +1,9 @@
-# Input two strings
 s1 = input("Enter first string: ")
 s2 = input("Enter second string: ")
 
-# Remove spaces and convert to lowercase
 s1 = s1.replace(" ", "").lower()
 s2 = s2.replace(" ", "").lower()
 
-# Check anagram
 if sorted(s1) == sorted(s2):
     print("The strings are anagrams.")
 else:
